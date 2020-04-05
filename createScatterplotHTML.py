08a704407900e745a7847c2746045ccd60b6438d
Claire Murphy <47284954+camurphy408@users.noreply.github.com>
2020-04-04 23:05:54 -0700

    Update createScatterplotHTML.py

--- createScatterplotHTML.py
+++ createScatterplotHTML.py
@@ -1,3 +1,5 @@
+# Requires installation of plotly, pandas, and numpy
+
 import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
@@ -6,10 +8,10 @@ fig = go.Figure()
 
 ourLine = pd.DataFrame(columns = ["x_values", "y_values"])
 ourLine.set_index("x_values")
-ourLine.at[latest, "x_values"] = 0
-ourLine.at[latest, "y_values"] = 0
-ourLine.at[latest, "x_values"] = 1
-ourLine.at[latest, "y_values"] = 1
+ourLine.at[0, "x_values"] = 0
+ourLine.at[0, "y_values"] = 0
+ourLine.at[1, "x_values"] = 1
+ourLine.at[1, "y_values"] = 1
 
 fig.add_trace(go.Scatter(x = ourLine["x_values"], y = ourLine["y_values"], mode = 'lines+markers', name = 'LSRL'))
 fig.write_html('graph.html')
